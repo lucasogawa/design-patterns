7906e80abc4e21c79e4f792a97018df72bb03556
Lucas Akio Ogawa <ogawalucas@gmail.com>
2023-05-22 06:19:02 -0300

    DESIGN PATTERN - Adding Prototype Pattern

---
+++ solid/src/com/ogawalucas/designpattern/creational/prototype/Rectangle.java
@@ -0,0 +1,18 @@
+package com.ogawalucas.designpattern.creational.prototype;
+
+public class Rectangle extends Shape {
+
+    public Rectangle(int width, int height) {
+        super(width, height);
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Rectangle with width: " + width + " height: " + height);
+    }
+
+    @Override
+    public Shape cloneObject() {
+        return new Rectangle(width, height);
+    }
+}
---
+++ solid/src/com/ogawalucas/designpattern/creational/prototype/Shape.java
@@ -0,0 +1,16 @@
+package com.ogawalucas.designpattern.creational.prototype;
+
+public abstract class Shape {
+
+    protected int width;
+    protected int height;
+
+    public Shape(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public abstract void draw();
+
+    public abstract Shape cloneObject();
+}
